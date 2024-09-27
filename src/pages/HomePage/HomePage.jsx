45842f83e068879feb78d868a36933fe74f8d307
rathoraryann <arrathor1334@gmail.com>
2024-09-27 14:35:12 +0530

    images added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Box } from "@chakra-ui/react"
+import { Container, Flex, Box, Text } from "@chakra-ui/react"
 import FeedPosts from "../../components/FeedPosts/FeedPosts"
 import SuggestedUsers from '../../components/SuggestedUsers/SuggestedUsers'
 
@@ -6,7 +6,7 @@ function HomePage() {
   return (
     <Container maxW={'container.lg'}>
       <Flex gap={20}>
-        <Box flex={2} py={10} >          
+        <Box flex={2} py={10} display={{base:'none', lg: 'block'}}>     
           <FeedPosts/>
         </Box>
         <Box flex={3} mr={20} display={{base:'none', lg:'block'}} >
